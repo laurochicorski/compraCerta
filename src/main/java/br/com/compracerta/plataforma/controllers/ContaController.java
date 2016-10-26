5ad72e2574bfb61587cd6d7c046a637b546ebfb0
Lauro Chicorski <lauro.chicorski@sankhya.com.br>
2016-10-26 13:31:25 -0200

    imp

---
+++ src/main/java/br/com/compracerta/plataforma/controllers/ContaController.java
@@ -0,0 +1,25 @@
+package br.com.compracerta.plataforma.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.compracerta.plataforma.entities.Conta;
+import br.com.compracerta.plataforma.repositories.ContaRepository;
+
+@Controller
+@RequestMapping("/conta")
+public class ContaController {
+
+	@Autowired
+	private ContaRepository contaRepository;
+	
+	@RequestMapping(value="/all", method= RequestMethod.GET)
+	public List<Conta> ConsultarTodos(){
+		return contaRepository.findAll();
+	}
+}
---
+++ src/main/java/br/com/compracerta/plataforma/repositories/ContaRepository.java
@@ -0,0 +1,31 @@
+package br.com.compracerta.plataforma.repositories;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.compracerta.plataforma.entities.Conta;
+
+@Repository
+public class ContaRepository {
+
+	@PersistenceContext
+	private EntityManager	manager;
+
+	public List<Conta> findAll() {
+		return manager.createQuery("select c from conta c", Conta.class).getResultList();
+	}
+
+	public Conta findById(Long id) {
+		return manager.createQuery("select * from conta c where c.id = :id", Conta.class).setParameter("id", id).getSingleResult();
+	}
+
+	@Transactional
+	public void save(Conta conta) {
+		manager.merge(conta);
+	}
+}
